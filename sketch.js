ab06bcef8ba7bc005d5b0d7ac9ef1364a00345d8
PrishaSusanne <62793055+PrishaSusanne@users.noreply.github.com>
2020-04-01 08:39:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var mouse;
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  mouse.x = mirrorX;
+
+if (mousePressedOver(mouse)) {
+  background(rgb(25,10,5));
+} else {
+  background("white");
+}
+}
+
+
+
+  function draw(){
+
+background("white");
+
+
+
+  }
